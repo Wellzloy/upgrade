ca27a4a73d2d246d746a0ecc9a348c1845cb462c
Wellzloy <well1970@mail.ru>
2024-09-26 23:56:35 +0400

    26/09

--- module_5_3.py
+++ module_5_3.py
@@ -20,6 +20,18 @@ class House:
     def __eq__(self, other):
         return (self.number_of_floors == other.number_of_floors)
 
+    def __add__(self, value):
+        return self.number_of_floors + value
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+
+
+
+
+
+
     def __lt__(self, other):
         return (self.number_of_floors < other.number_of_floors)
 
@@ -35,27 +47,58 @@ class House:
     def __le__(self, other):
         return (self.number_of_floors != other.number_of_floors)
 
-    def __add__(self, value):
-        pass
 
 
-# Методы __lt__(<), __le__(<=), __gt__(>), __ge__(>=), __ne__(!=)
+
+
+
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 
-# __str__
 print(h1)
 print(h2)
 
-# __len__
-print(len(h1))
-print(len(h2))
-
-print(h1 == h2)
-print(h1 < h2)
-print(h1 <= h2)
-print(h1 > h2)
-print(h1 >= h2)
-print(h1 != h2)
+print(h1 == h2) # __eq__
 
 
+print(h1)
+h1 = h1 + 10 # __add__
+print(h1)
+
+
+#print(h1 == h2)
+
+#h1 += 10 # __iadd__
+#print(h1)
+
+# h2 = 10 + h2 # __radd__
+# print(h2)
+
+# print( h1 > h2) # __gt__
+# print(h1 >= h2) # __ge__
+# print(h1 < h2) # __lt__
+# print(h1 <= h2) # __le__
+# print(h1 != h2) # __ne__
+
+
+class Count(object):
+    def __init__(self, count):
+        self._count = count
+
+    def __add__(self, other):
+        total_count = self._count + other._count
+        return Count(total_count)
+
+    def __radd__(self, other):
+        if other == 0:
+            return self
+        else:
+            return self.__add__(other)
+
+    def __str__(self):
+        return 'Count:% i' % self._count
+
+
+c2 = Count(2)
+c3 = 0 + c2
+print(c3)
