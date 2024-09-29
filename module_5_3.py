f3944da408d248a203cc7ba59fa939fe389511de
Wellzloy <well1970@mail.ru>
2024-09-29 14:44:31 +0400

    29/09 ok

--- module_5_3.py
+++ module_5_3.py
@@ -1,5 +1,6 @@
 class House:
     def __init__(self, name, number_of_floors):
+        self.new_floor = None
         self.name = name
         self.number_of_floors = number_of_floors
 
@@ -18,7 +19,7 @@ class House:
         return f'Название: {self.name}, количество этажей: {self.number_of_floors} '
 
     def __eq__(self, other):
-        return (self.number_of_floors == other.number_of_floors)
+        return self.number_of_floors == other.number_of_floors
 
     def __add__(self, value):
         self.number_of_floors = self.number_of_floors + value
@@ -33,39 +34,36 @@ class House:
         return self
 
     def __gt__(self, other):
-        return (self.number_of_floors > other.number_of_floors)
+        return self.number_of_floors > other.number_of_floors
 
     def __ge__(self, other):
-        return (self.number_of_floors >= other.number_of_floors)
+        return self.number_of_floors >= other.number_of_floors
 
     def __lt__(self, other):
-        return (self.number_of_floors < other.number_of_floors)
+        return self.number_of_floors < other.number_of_floors
 
     def __le__(self, other):
-        return (self.number_of_floors <= other.number_of_floors)
+        return self.number_of_floors <= other.number_of_floors
 
     def __ne__(self, other):
-        return (self.number_of_floors != other.number_of_floors)
-
+        return self.number_of_floors != other.number_of_floors
 
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 print(h1)
 print(h2)
-print(len(h1)) # __len__
-print(h1 == h2) # __eq__
-h1 = h1 + 10 # __add__
+print(len(h1))  # __len__
+print(h1 == h2)  # __eq__
+h1 = h1 + 10  # __add__
 print(h1)
 print(h1 == h2)
-h1 += 10 # __iadd__
+h1 += 10  # __iadd__
 print(h1)
-h2 = 10 + h2 # __radd__
+h2 = 10 + h2  # __radd__
 print(h2)
-print(h1 > h2) # __gt__
-print(h1 >= h2) # __ge__
-print(h1 < h2) # __lt__
-print(h1 <= h2) # __le__
-print(h1 != h2) # __ne__
-
-
+print(h1 > h2)  # __gt__
+print(h1 >= h2)  # __ge__
+print(h1 < h2)  # __lt__
+print(h1 <= h2)  # __le__
+print(h1 != h2)  # __ne__
