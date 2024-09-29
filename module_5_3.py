2bd80093f8fc1307b138b1b51427b7b32cefac29
Wellzloy <well1970@mail.ru>
2024-09-29 14:30:43 +0400

    29/09

--- module_5_3.py
+++ module_5_3.py
@@ -21,22 +21,16 @@ class House:
         return (self.number_of_floors == other.number_of_floors)
 
     def __add__(self, value):
-        return self.number_of_floors + value
+        self.number_of_floors = self.number_of_floors + value
+        return self
+
+    def __iadd__(self, value):
+        self.number_of_floors += value
+        return self
 
     def __radd__(self, value):
-        return self.__add__(value)
-
-
-
-
-
-
-
-    def __lt__(self, other):
-        return (self.number_of_floors < other.number_of_floors)
-
-    def __le__(self, other):
-        return (self.number_of_floors <= other.number_of_floors)
+        self.number_of_floors = value + self.number_of_floors
+        return self
 
     def __gt__(self, other):
         return (self.number_of_floors > other.number_of_floors)
@@ -44,61 +38,34 @@ class House:
     def __ge__(self, other):
         return (self.number_of_floors >= other.number_of_floors)
 
+    def __lt__(self, other):
+        return (self.number_of_floors < other.number_of_floors)
+
     def __le__(self, other):
+        return (self.number_of_floors <= other.number_of_floors)
+
+    def __ne__(self, other):
         return (self.number_of_floors != other.number_of_floors)
-
-
-
 
 
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
-
 print(h1)
 print(h2)
-
+print(len(h1)) # __len__
 print(h1 == h2) # __eq__
-
-
-print(h1)
 h1 = h1 + 10 # __add__
 print(h1)
+print(h1 == h2)
+h1 += 10 # __iadd__
+print(h1)
+h2 = 10 + h2 # __radd__
+print(h2)
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
 
 
-#print(h1 == h2)
-
-#h1 += 10 # __iadd__
-#print(h1)
-
-# h2 = 10 + h2 # __radd__
-# print(h2)
-
-# print( h1 > h2) # __gt__
-# print(h1 >= h2) # __ge__
-# print(h1 < h2) # __lt__
-# print(h1 <= h2) # __le__
-# print(h1 != h2) # __ne__
-
-
-class Count(object):
-    def __init__(self, count):
-        self._count = count
-
-    def __add__(self, other):
-        total_count = self._count + other._count
-        return Count(total_count)
-
-    def __radd__(self, other):
-        if other == 0:
-            return self
-        else:
-            return self.__add__(other)
-
-    def __str__(self):
-        return 'Count:% i' % self._count
-
-
-c2 = Count(2)
-c3 = 0 + c2
-print(c3)
