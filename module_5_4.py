301f3798298b8c878e39c339eb6f52d4cec93903
Wellzloy <well1970@mail.ru>
2024-09-29 23:29:04 +0400

    29/09 2 step

--- module_5_4.py
+++ module_5_4.py
@@ -59,10 +59,19 @@ class House:
     def __ne__(self, other):
         return self.number_of_floors != other.number_of_floors
 
+    def __del__(self):
+        print(f'{self.name} снесён, но он останется в истории')
+
 
 h1 = House('ЖК Эльбрус', 10)
 print(House.houses_history)
 h2 = House('ЖК Акация', 20)
 print(House.houses_history)
 h3 = House('ЖК Матрёшки', 20)
+
+print(House.houses_history)
+
+del h2
+del h3
+
 print(House.houses_history)
