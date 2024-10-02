0b9ac56aba731b4c58ccfef969e19a31f4e57920
Wellzloy <well1970@mail.ru>
2024-10-02 21:38:09 +0400

    02/10 end
    Signed-off-by: Wellzloy <well1970@mail.ru>

---
+++ login_password.py
@@ -0,0 +1,26 @@
+class Database:
+    def __init__(self):
+        self.data = {}
+
+    def add_user(self, username, password):
+        self.data[username] = password
+
+class User:
+    """
+    Класс пользователя, содержащий атрибуты: логин и пароль
+    """
+
+    def __init__(self, username, password, password_confirm):
+        self.username = username
+        if password == password_confirm:
+            self.password = password
+
+if __name__ == '__main__':
+    database = Database()
+    while True:
+        choice = input('Приветсвую! Выбирите: \n 1 Вход\n 2 Регистрация\n')
+        user = User(input('Введите логин: '), password :=input('Введите пароль: '), password2 :=input('Повторите пароль: '))
+        if password != password2:
+            exit()
+        database.add_user(user.username, user.password)
+        print(database.data)
