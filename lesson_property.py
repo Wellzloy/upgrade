ad8fc4f5a229d1381701438f043025f015bd26f7
Wellzloy <well1970@mail.ru>
2024-10-03 21:32:53 +0400

    03/10 end
    Signed-off-by: Wellzloy <well1970@mail.ru>

--- lesson_property.py
+++ lesson_property.py
@@ -23,15 +23,13 @@ class Box:
     material = 'peiper'
 
     def __init__(self, x, y, z):
-        if not isinstance(x, int):
-            raise TypeError('length must be "int"')
-        elif x > 0:
-            self.length = x
-        else:
-            raise ValueError(f'length more then 0, got: {x = }')
-        self.width = y
-        self.height = z
-        self.volume = self.height * self.width * self.height
+        # if not isinstance(x, int):
+        #     raise TypeError('length must be "int"')
+        # elif x > 0:
+        #     self.__length = x
+        # else:
+        #     raise ValueError(f'length more then 0, got: {x = }')
+        self.set_length(x)
 
         if not isinstance(y, int):
             raise TypeError('length must be "int"')
@@ -48,31 +46,26 @@ class Box:
             raise ValueError(f'length more then 0, got: {z = }')
 
 
-        self.volume = self.height * self.width * self.height
-
+        self.volume = self.__length * self.width * self.height
 
     def get_length(self):
-        return self.length
+        return self.__length
 
     def set_length(self, value):
         if not isinstance(value, int):
             raise TypeError('length must be "int"')
         elif value > 0:
-            self.length = value
+            self.__length = value
         else:
             raise ValueError(f'length more then 0, got: {value = }')
 
-
-
-
-
 box_1 = Box(1, 2, 3)
 # box_1.length = -18
-# box_1.set_length(-18)
-box_1.length = -15
+box_1.set_length(10)
+
 print(box_1.get_length())
 print(box_1.__dict__)
-print(box_1.__class__)
-box_1 = Box(10, 2, 3)
-print(box_1.__dict__)
-print(box_1.__class__)
+# print(box_1.__class__)
+# box_1 = Box(10, 2, 3)
+# print(box_1.__dict__)
+# print(box_1.__class__)
