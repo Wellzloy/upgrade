474c7eba96064dfa731cbc2b936eae05f97161a6
Wellzloy <93443716+Wellzloy@users.noreply.github.com>
2024-10-28 15:59:25 +0300

    Add files via upload

--- explorer.py
+++ explorer.py
@@ -1,21 +1,26 @@
 import tkinter
-
+import os
 from tkinter import filedialog
 
+
 def file_select():
-    filename = filedialog.askopenfile(initialdir='/', title='Выбирите файл',
+    filename = filedialog.askopenfilename(initialdir='/', title='Выбирите файл',
                                       filetypes=(('Текстовый файл', '.txt'),
                                                  ('Все файлы', '*')))
+    text['text'] = text['text'] + '' + filename
+    os.startfile(filename)
+
 
 window = tkinter.Tk()
 window.title('Проводник')
-window.geometry('350x150')
+window.geometry('450x150')
 window.configure(bg='black')
 window.resizable(False, False)
-text = tkinter.Label(window, text='Файл:', height=5, width=50, background='silver',
+text = tkinter.Label(window, text='Файл:', height=5, width=65, background='silver',
                      foreground='blue')
 text.grid(column=1, row=1)
 button_select = tkinter.Button(window, width=20, height=3, text='Выбрать файл',
-                               background='silver', foreground='blue', command=file_select)
+                               background='silver', foreground='blue',
+                               command=file_select)
 button_select.grid(column=1, row=2)
 window.mainloop()
